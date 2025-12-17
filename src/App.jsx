af6719440d0812a43cbd9c90f428394adb4d273d
Amal A M <vectorhero007@gmail.com>
2025-12-17 11:33:29 +0530

    proper routes structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Nav from "./Nav";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
 
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className='App'>
-      
+    <div className="App">
+      <Header />
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<NewPost />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
